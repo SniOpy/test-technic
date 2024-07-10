b107acc600a9c1b3ef6c5ff223671d398587a361
SniOpy <soufianeibna@gmail.com>
2024-07-10 21:30:33 +0200

    Ajout de MUI + affichage côté front

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Test technic
 Application web qui affiche nos différents produits, avec la possibilité de modifier, supprimer ou même créer un nouveau produits
+
+# Pré-requis
+Positionner vous sur le dossier back en tapant 
+```
+cd back
+```
+lancer le script bash afin de créer le rôle, la database, créer les tables et importer la data
+
+```bash
+bash ./scripts/deploy_db.sh
+```
+
+# Connexion API back 
+ Tapez sur votre terminal 
+
+ ```
+cd back
+
+npm install
+npm run dev 
+ ```
+
+ # Lancer l'application 
+
+ Version node supérieur à v18
+
+ ```
+ cd front
+ npm install
+ npm run dev
+ 
+ ```
+
--- front/src/App/App.jsx
+++ front/src/App/App.jsx
@@ -1,5 +1,21 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import * as React from 'react';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { useState, useEffect } from 'react';
+
+
+
 
 const App = () => {
 
@@ -7,6 +23,16 @@ const App = () => {
 
     const [products,setProducts] = useState([]);
   //! Comportements
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+  
   const getProducts = () => {
 
     fetch("http://localhost:5000/products")
@@ -22,16 +48,45 @@ const App = () => {
   //! Affichage
   return (
     <div className="App">
+      <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
       <h1>Bon artisan </h1>
       <h3>Nos produits</h3>
-
-      <ul>
+      <Box sx={{ width: '100%' }}>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {products.map((product) => {
           return (
-            <li key={product.id}>{product.name_product}</li>
+            
+              <Grid key={product.id}  item xs={6}>
+                <Item>
+                <Card key={product.id} sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image="https://picsum.photos/200/300/?greyscale"
+                      title={product.name_product}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                       {product.name_product}
+                      </Typography>
+                      
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" variant="contained">Afficher</Button>
+                      <Button size="small" variant="outlined">Supprimer</Button>
+                    </CardActions>
+                  </Card>
+                </Item>
+              </Grid>
+          
           )
         })}
-      </ul>
+          </Grid>
+          </Box>
+          </Container>
+          </React.Fragment>
+      
     </div>
   )
 }
